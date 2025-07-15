8bc27f3c6c58dc56dbb8d4cfa0b7a60ca54dc7ab
JSQdev <148073798+devJSQ@users.noreply.github.com>
2025-07-15 05:19:56 +0300

    Update Tag.java

--- src/Tag.java
+++ src/Tag.java
@@ -1,5 +1,16 @@
-class Tag {
+import java.util.*;
 
-  /* write your code here */
+public class Tag {
+    public String name;
+    public List<String[]> attributes = new ArrayList<>();
 
-}  
+    public Tag(String name) {
+        this.name = name;
+    }
+    public void addAttribute(String key, String value) {
+        attributes.add(new String[]{key, value});
+    }
+    public String toString() {
+        return "<" + name + ">";
+    }
+}
